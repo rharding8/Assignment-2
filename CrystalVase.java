0ba3e878f7682ab69ce25751497f8b9ebb822068
rharding8 <ry287616@ucf.edu>
2024-02-23 23:24:59 -0500

    Added Problem 2 stuff

---
+++ CrystalVase.java
@@ -0,0 +1,71 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class CrystalVase {
+    public static void main(String[] args) {
+        VaseRoom vr = new VaseRoom(1);
+        vr.vaseViewing();
+    }
+}
+
+class VaseRoom {
+    private Queue<Guest> queue = new LinkedList<>();
+    private Random rand = new Random();
+    private int numGuests;
+
+    public VaseRoom(int n) {
+        numGuests = n;
+    }
+
+    public synchronized void vase(int id) {
+        System.out.println("Guest " + id + " is looking at the vase!");
+    }
+
+    class Guest extends Thread {
+        private int id;
+
+        public Guest(int n) {
+            id = n;
+        }
+
+        @Override
+        public void run() {
+            do {
+                try {
+                    synchronized(this) {
+                        wait();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("ERROR!");
+                    e.printStackTrace();
+                    return;
+                }
+
+                vase(id);
+                queue.remove(this);
+                System.out.println("Removed!");
+                if (rand.nextBoolean()) {
+                    queue.add(this);
+                    System.out.println("Added!");
+                }
+            } while (queue.contains(this));
+        }
+    }
+
+    public void vaseViewing() {
+        for (int i = 0; i < numGuests; i++) {
+            Guest g = new Guest(i);
+            g.start();
+            queue.add(g);
+        }
+
+        while (!queue.isEmpty()) {
+            if (queue.peek() != null) {
+                synchronized(queue.peek()) {
+                    queue.peek().notify();
+                }
+            }
+        }
+    }
+}
